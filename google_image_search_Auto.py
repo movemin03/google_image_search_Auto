296baf215942f0266b3aa0b9b0308fe2b60d773e
DONGMIN LEE <movemin03@naver.com>
2024-05-01 17:49:43 +0900

    240501
    유사도 검색 기능 추가

--- google_image_search_Auto.py
+++ google_image_search_Auto.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from PIL import Image
 import shutil
 from selenium import webdriver
@@ -10,9 +11,13 @@ import subprocess
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pywinauto.application import Application
+import io
+import base64
+import cv2
+from skimage.metrics import structural_similarity as ssim
 
 source_dir = input("사진이 들어있는 폴더 경로를 넣어주세요(put your folder path):").replace("'", "").replace('"', "")
-dest_dir = source_dir + "\사진변환"
+dest_dir = source_dir + "\\converted"
 
 def chk_dest_dir(dest_dir):
     if not os.path.exists(dest_dir):
@@ -33,18 +38,28 @@ def check_size(file):
     return os.path.getsize(file)
 
 # 이미지 압축
-def compress_image(image_path, output_folder, desired_size):
+def compress_image(image_path, output_folder, desired_size, idx):
     img = Image.open(image_path)
-    img.save(f"{output_folder}\\{os.path.basename(image_path)}", quality=90, optimize=True)
+    img.save(f"{output_folder}\\{idx}.jpg", quality=90, optimize=True)
+
+index_table = []
+
 
 # 파일 처리
-def process_files(source_folder, dest_folder):
+def process_files(source_dir, dest_dir):
     print("처리를 시작합니다. 별도의 안내가 있을 때까지 종료하지 마십시오")
-    for foldername, subfolders, filenames in os.walk(source_folder):
+    for foldername, subfolders, filenames in os.walk(source_dir, topdown=True):
+        # 하위 폴더를 제외하도록 설정
+        subfolders.clear()
+        idx = 1
+        # 이제 해당 폴더에 대해서만 반복문 실행
         for filename in filenames:
-            if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'): # 이미지 파일인 경우
+            if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'):
+                # 여기서 필요한 작업을 수행
                 file_path = os.path.join(foldername, filename)
-                compress_image(file_path, dest_folder, 195) # 이미지 압축
+                compress_image(file_path, dest_dir, 195, idx)  # 이미지 압축
+                index_table.append((file_path, idx))
+                idx += 1
 
 # 실행
 print("이미지 전처리 중입니다\n이미지가 많을 시 처리에 시간이 걸릴 수 있습니다")
@@ -62,8 +77,51 @@ driver = webdriver.Chrome(options=option)
 driver.get("https://images.google.com/")
 
 # 리스트 초기화
-filename = []
-url = []
+filename_list = []
+url_list = []
+similarity_list = []
+
+
+
+def calculate_ssim(image1, image2):
+    # 이미지를 그레이스케일로 변환
+    gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+
+    # 유사도 계산
+    (_, similarity) = ssim(gray1, gray2, full=True)
+    return similarity
+
+
+def base64_to_image(base64_string):
+    # base64 문자열을 이미지로 디코딩
+    image_data = base64.b64decode(base64_string)
+    image = Image.open(io.BytesIO(image_data))
+    return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+
+def image_similarity(base64_string, path):
+    # base64 이미지를 이미지로 변환
+    base64_image = base64_to_image(base64_string)
+    # 파일에서 이미지를 읽어옴
+    image2 = cv2.imread(path)
+
+    width = min(base64_image.shape[1], image2.shape[1])
+    height = min(base64_image.shape[0], image2.shape[0])
+
+    base64_image = cv2.resize(base64_image, (width, height))
+    image2 = cv2.resize(image2, (width, height))
+
+    # 이미지 유사도 계산
+    similarity_matrix = calculate_ssim(base64_image, image2)
+    similarity = np.mean(similarity_matrix)
+    return similarity
+
+def get_value_by_index(index_table, idx):
+    for item in index_table:
+        if item[1] == idx:
+            return item[0]
+    return None  # 인덱스에 해당하는 값이 없는 경우 None 반환
 
 # 함수 정의
 def find_image_url(path):
@@ -90,9 +148,31 @@ def find_image_url(path):
     # URL 갱신 확인 후 저장
     time.sleep(2)  # 페이지 로딩 대기
     new_url = driver.current_url
-    url.append(new_url)
-    filename.append(os.path.basename(path))
-    print("처리 완료")
+    url_list.append(new_url)
+
+    filename_list.append(get_value_by_index(index_table, int(os.path.splitext(os.path.basename(path))[0])))
+    print("검색 url 저장 완료")
+    time.sleep(1)
+
+    while True:
+        try:
+            searched_img = driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/div/div[1]/div/div[2]/div/img')
+            break
+        except:
+            # 요소를 찾지 못한 경우, 예외 처리 후 다시 시도
+            print("이미지 업로드를 제대로 완료한 후 엔터")
+            a = input()
+            continue
+    searched_img = driver.find_element(By.XPATH,'//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/c-wiz/div/div[1]/div/div[2]/div/img')
+    base64_string = searched_img.get_attribute("src")
+    base64_string = base64_string.split(',')[1]
+    if base64_string:
+        similarity = image_similarity(base64_string, path) * 100
+        print("유사도:", similarity)
+    else:
+        similarity = 0
+        print("유사도를 검색할 수 없습니다")
+    similarity_list.append(similarity)
 
 upper_path = dest_dir
 
@@ -103,8 +183,17 @@ for file in os.listdir(upper_path):
         find_image_url(upper_path + "\\" + file)
 
 # 데이터프레임 생성 및 저장
-df = pd.DataFrame({'filename': filename, 'url': url})
-df.to_excel("C:\\Users\\" + user + "\\Desktop\\img_links.xlsx", index=False)
+df = pd.DataFrame({'filename': filename_list, 'url': url_list, '유사도': similarity_list})
+while True:
+    try:
+        df.to_excel(f"C:\\Users\\{user}\\Desktop\\img_links.xlsx", index=False)
+        print("엑셀 파일이 성공적으로 저장되었습니다.")
+        break  # 성공적으로 저장되었으므로 반복문 종료
+    except Exception as e:
+        print(f"엑셀 파일 저장 중 오류가 발생했습니다: {e}")
+        print("파일이 열려있는 경우 종료해주시고 엔터")
+        a = input()
+        continue  # 예외가 발생하면 다시 시도
 print("바탕화면에 img_links.xlsx 이름으로 저장되었습니다")
 # 웹드라이버 종료
 driver.quit()
